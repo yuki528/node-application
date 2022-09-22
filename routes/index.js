a13c5f502fb65e93244cf73ac8299c0757b33ec3
Yuki528 <kiplimomeli44@gmail.com>
2022-09-22 11:35:46 +0300

    progress on routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('The routes work!!!');
+    res.render('form');
 })
 
 module.exports = router;
